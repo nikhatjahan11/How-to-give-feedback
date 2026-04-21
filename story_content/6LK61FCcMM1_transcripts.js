7fd491c8b0a93263cb44820a0c19ce9b668d3a39
nikhatjahan11 <153170960+nikhatjahan11@users.noreply.github.com>
2026-04-21 19:05:20 +0530

    adding course files

---
+++ story_content/6LK61FCcMM1_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":490,"text":"Why am I always late?"},{"start":2809,"text":"Not again."},{"start":4078,"text":"Prepare properly."},{"start":7239,"text":"Mhm"}]}]};
+    window.globalLoadJsAsset('story_content/6LK61FCcMM1_transcripts.js', JSON.stringify(data));
+})();
